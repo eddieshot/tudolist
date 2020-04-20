7de078f228590fb562d8d760d7d489e6705d4c7d
lookfire <renancontiero@hotmail.com>
2020-04-20 14:33:18 -0300

    implementado o additem.component.ts e iniciado a estilização do componente.

--- todoListApp/src/app/app.component.ts
+++ todoListApp/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoListApp';
+  title = 'Ajustar Tarefas';
+  titleIndex = 'Listas de Tarefas'
 }
--- todoListApp/src/app/app.module.ts
+++ todoListApp/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DisplayComponent } from './display/display.component';
+
 import { AdditemComponent } from './additem/additem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayComponent,
     AdditemComponent
   ],
   imports: [
