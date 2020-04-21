075b8e8c6872965e45edae2ecf3d46ee4cd8e399
Raul Ferreira <raul.ferreira.2@hotmail.com>
2020-04-21 12:55:46 -0300

    novas funções e data binding de componentes

--- todoListApp/src/app/additem/additem.component.ts
+++ todoListApp/src/app/additem/additem.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdditemComponent implements OnInit {
 
+
+  nomeTarefa:string;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  arrayLista:Array<any> =[
+    "Lista item 1",
+    "lista item 2"
+  ]
+
+  guardaTarefa(event){
+    this.nomeTarefa = event.target.value
+  }
+
+  adicionarItem(){
+    this.arrayLista.unshift(this.nomeTarefa)
   }
 
 }
