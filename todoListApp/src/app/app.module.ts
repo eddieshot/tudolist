31472b6725522c03c6f28d0bc907ce7d634fe648
Raul Ferreira <raul.ferreira.2@hotmail.com>
2020-04-21 12:55:23 -0300

    reajuste do app component

--- todoListApp/src/app/app.module.ts
+++ todoListApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AdditemComponent } from './additem/additem.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdditemComponent
+    AdditemComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
