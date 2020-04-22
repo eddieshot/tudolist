b09e8f30a4ad99eac26ef6a0a14a1bf6e49852be
Raul Ferreira <raul.ferreira.2@hotmail.com>
2020-04-22 14:33:39 -0300

    ajustes no display

--- todoListApp/src/app/display/display.component.ts
+++ todoListApp/src/app/display/display.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DisplayComponent implements OnInit {
 
+  status:boolean;
   title = 'Ajustar Tarefas';
   show: boolean = true;
 
